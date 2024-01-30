2f053f6b3854eaaffc451e824b4b2e4b0098654e
ih8josh <152526120+ih8josh@users.noreply.github.com>
2024-01-30 15:42:00 -0600

    jale

---
+++ server.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cfonts = require('cfonts');
+
+// create a MySQL connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employeeTracker_db",
+});
