c3a9815b7d0dd6745da1691b048b161cc33ce075
ih8josh <152526120+ih8josh@users.noreply.github.com>
2024-01-30 16:01:27 -0600

    barranco

--- server.js
+++ server.js
@@ -10,3 +10,24 @@ const connection = mysql.createConnection({
     password: "",
     database: "employeeTracker_db",
 });
+function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Add a Manager",
+                "Update an employee role",
+                "View Employees by Manager",
+                "View Employees by Department",
+                "Delete Departments | Roles | Employees",
+                "View the total utilized budget of a department",
+                "Exit",
+            ],
